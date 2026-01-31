a94b37c039096104e6084ff58c348928f48d3ade
Ruby kushwah <rkushwah9826@gmail.com>
2026-01-31 21:40:51 +0530

    744

---
+++ 01-DSA-Java/Arrays/744.java
@@ -0,0 +1,13 @@
+public class 744 {
+    class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] > target) {
+                return letters[i];
+            }
+        }
+        return letters[0];
+    }
+}
+
+}
