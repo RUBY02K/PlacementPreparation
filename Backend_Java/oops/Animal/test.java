c70286fd1e984e6e505a41f34398f60f58892c9e
Ruby kushwah <rkushwah9826@gmail.com>
2026-02-18 17:40:14 +0530

    abstraction and polymorphism

---
+++ Backend_Java/oops/Animal/test.java
@@ -0,0 +1,15 @@
+import Backend_Java.oops.human.child;
+
+public class test {
+    public static void main(String[] args) {
+        // Dog dog = new Dog();
+        // dog.setAge(3);
+        // dog.setName("bob");
+        // dog.getName();
+        // dog.getAge();
+        // animal animal = new animal();
+        // animal.sayhello();
+        // dog.sayhello();
+         
+    }
+}
--- Backend_Java/oops/abstraction/test.java
+++ Backend_Java/oops/abstraction/test.java
@@ -2,7 +2,7 @@ package abstraction;
 
 public class test {
     public static void main(String[] args) {
-        animal animal = new animal(); // abstract class ka constructor cannnot be initialised or made
+//        animal animal = new animal(); // abstract class ka constructor cannnot be initialised or made
         animal dog = new dog();
         animal cat = new cat ();
         cat.sayhello();
