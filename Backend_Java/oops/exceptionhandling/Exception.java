7675bacbfee680639fe31264c4bc8aae2781272d
Ruby kushwah <rkushwah9826@gmail.com>
2026-02-25 07:30:16 +0530

    Exception handling

--- Backend_Java/oops/exceptionhandling/Exception.java
+++ Backend_Java/oops/exceptionhandling/Exception.java
@@ -1,21 +1,33 @@
 package exceptionhandling;
 
+
+import accessmodifiers.test.Student;
+
 public class Exception {
- public static  void main (String[] args){
-     int [] numerator = {12,34,56,30};
-     int []denominator = {2,3,0,4};
-     for (int i = 0; i< numerator.length; i++){
-     System.out.println(divide(numerator[i], denominator[i]));
-     }
-     System.out.println("good job ");
+    public static void main(String[] args) {
+        int[] numerator = {12, 34, 56, 30};
+        int[] denominator = {2, 3, 0, 4};
+        for (int i = 0; i < 10; i++) {
+            try{
+                System.out.println(divide(numerator[i], denominator[i]));
+
+            }catch (java.lang.Exception e ){
+                System.out.println(e);
+            }
+        }
+        System.out.println("good job :) ");
     }
- private static int divide (int a , int b ){
-     try{
-         return a/b;
-     }catch (ArithmeticException e){
-         return -1;
-     }
 
+    private static int divide(int a, int b) {
+        try {
+            return a / b;
+        }  catch (ArithmeticException e) {
+            System.out.println("arithimatic exception");
+            return -1;
+        } catch (java.lang.Exception e) {
+            System.out.println(e);
+            return -1;
 
- }
+        }
+    }
 }
