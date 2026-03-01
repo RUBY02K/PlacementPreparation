4901c564d0321a73f2c04024b56b4cfaf68e4ceb
Ruby kushwah <rkushwah9826@gmail.com>
2026-03-01 21:39:19 +0530

    wrapper classes

---
+++ Backend_Java/oops/wrapperclass/math.java
@@ -0,0 +1,16 @@
+package wrapperclass;
+
+public class math {
+   public static void main(String[] args) {
+ int a = 20 ; int b = 59 ;
+       int max = Math.max(a, b);
+       int min = Math.min(a, b);
+       int c = -11;
+       Math.abs(c);
+       double d = 1.12;
+       double ceil = Math.ceil(d);
+       System.out.println(ceil);
+
+   }
+
+}
---
+++ Backend_Java/oops/wrapperclass/test.java
@@ -0,0 +1,20 @@
+package wrapperclass;
+
+import java.util.ArrayList;
+
+public class test {
+     public static void main(String[] args) {
+//         int a = 3;
+//         Integer b = 4;
+//         boolean hasgraficcard = true ;
+//         Boolean isAdult = true ;
+//         Float f = 8.0f;
+//         Double d  = 56.89;
+//         String s = "ruby";
+//         Character c = 'r';
+//         Byte  bb = 1;
+//         Short ss = 2;
+//         Long l = 123l;
+
+    }
+}
