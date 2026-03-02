87265bb469eaffb0a64d744805de4efea7ee79a9
Ruby kushwah <rkushwah9826@gmail.com>
2026-03-02 15:25:24 +0530

    math class

--- Backend_Java/oops/wrapperclass/math.java
+++ Backend_Java/oops/wrapperclass/math.java
@@ -10,7 +10,22 @@ public class math {
        double d = 1.12;
        double ceil = Math.ceil(d);
        System.out.println(ceil);
+       System.out.println(Math.floor(d)); // koi number agr 1.12 hai toh uska floor hua 1.0 , ceil hua 2.0 /
+       System.out.println(Math.round(d));
+
+       int e = 144 ;
+       System.out.println(Math.sqrt(e));
+       System.out.println(Math.pow(12 , 3));
+
+       System.out.println(Math.log10(10));
+
+       System.out.println("sin 90 is : "+ Math.sin(90));
+       System.out.println((int)(Math.random() * 11)); // any rando number between 0 to 11;
+
+       System.out.println(Math.nextAfter(1, 3 ));
+
 
    }
+
 
 }
