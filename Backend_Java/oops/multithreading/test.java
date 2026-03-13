5989d699d0be17d154810383625ae36b48e10786
Ruby kushwah <rkushwah9826@gmail.com>
2026-03-13 23:46:27 +0530

    multithresding

---
+++ Backend_Java/oops/multithreading/test.java
@@ -0,0 +1,11 @@
+package multithreading;
+
+public class test {
+    public static void main (String[] args)
+   {
+       System.out.println("hello world ");
+       System.out.println(Thread.currentThread().getName());
+
+    }
+
+}
