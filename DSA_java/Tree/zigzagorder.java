96e1aa5916f3e83e235da49ef04c455a46086388
Ruby kushwah <rkushwah9826@gmail.com>
2026-06-29 23:26:24 +0530

    LCAINbinarytree

--- DSA_java/Tree/zigzagorder.java
+++ DSA_java/Tree/zigzagorder.java
@@ -1,5 +1,48 @@
-package DSA_java.Tree;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
 
-public class zigzagorder {
-    
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        boolean ltr = true;
+
+        while (!q.isEmpty()) {
+            int levelsize = q.size();
+            List<Integer> temp = new ArrayList<>();
+
+            while (levelsize-- > 0) {
+                TreeNode t = q.poll();
+
+                if (ltr) {
+                    temp.add(t.val);
+                } else {
+                    temp.add(0, t.val); // reverse insert
+                }
+
+                if (t.left != null) q.offer(t.left);
+                if (t.right != null) q.offer(t.right);
+            }
+
+            ans.add(temp);
+            ltr = !ltr; // level ke baad flip
+        }
+
+        return ans;
+    }
 }
