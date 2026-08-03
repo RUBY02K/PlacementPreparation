3cc7c2be4d945611f9fb91aebcc8a92bb48fc6cb
Ruby kushwah <rkushwah9826@gmail.com>
2026-08-03 16:21:11 +0530

    stock3

---
+++ DSA_java/DP/stock3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProfit(int[] prices) { 
+        int n = prices.length;
+        int k = 2 ;
+        int dp[][] = new int[n+1][k+1];
+        for(int i = 0 ; i < n ; i ++){
+            Arrays.fill(dp[i],-1);
+            dp[i][0] = 0 ;
+           
+        }
+        for(int i = 0 ; i< k;i++){
+             dp[n][i]=  0;
+        }
+        for(int i = n-1 ; i >=0 ; i--){
+            for (int j = 1 ; j <k ; j++){
+                if(j==2){
+                  dp[i][j] = Math.max(dp[i+1][j-1]-prices[i] , dp[i+1][j]);
+                }
+                else {
+                  dp[i][j] = Math.max(dp[i+1][j-1]+prices[i] , dp[i+1][j]);  
+                }
+            }
+        }
+        return dp[0][2];
+    }
+}
