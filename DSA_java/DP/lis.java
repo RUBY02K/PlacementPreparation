b3848d9cab40da07ffd4bc2e000ca613b0dd9dec
Ruby kushwah <rkushwah9826@gmail.com>
2026-08-03 12:18:17 +0530

    longest increasing subsequence

---
+++ DSA_java/DP/lis.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+       int n = nums.length;
+       int res[] = new int[n];
+       for(int i = 0 ; i < n ; i++){
+        res[i]= 1;
+        for(int j = 0 ; j <i; j++){
+            if(nums[j] < nums[i]){
+            res[i]= Math.max(res[i] , res[j]+1);
+            }
+        }
+       }
+       int max = res[0];
+       for(int i = 0 ; i < n ; i++){
+         max = Math.max(res[i], max);
+        
+       }
+       return max;
+    }
+}
--- DSA_java/DP/partitionsum.java
+++ DSA_java/DP/partitionsum.java
@@ -1,1 +1,37 @@
+class Solution {
+    static boolean equalPartition(int arr[]) {
+
+        int n = arr.length;
+
+        int sum = 0;
+        for (int x : arr)
+            sum += x;
+
+        if (sum % 2 != 0)
+            return false;
+
+        int target = sum / 2;
+
+        int[][] dp = new int[n + 1][target + 1];
+
+        dp[n][0] = 1;
+        for (int j = 1; j <= target; j++)
+            dp[n][j] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= target; j++) {
+
+                int notTake = dp[i + 1][j];
+                int take = 0;
+
+                if (arr[i] <= j)
+                    take = dp[i + 1][j - arr[i]];
+
+                dp[i][j] = (take == 1 || notTake == 1) ? 1 : 0;
+            }
+        }
+
+        return dp[0][target] == 1;
+    }
+}
 
