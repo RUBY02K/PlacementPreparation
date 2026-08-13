5979e500bd0810ebe79c8d86c30c44a6945fc05e
Ruby kushwah <rkushwah9826@gmail.com>
2026-08-13 11:58:53 +0530

    subarray product except self

--- DSA_java/Arrays/longestsubstringwithkleastfreq.java
+++ DSA_java/Arrays/longestsubstringwithkleastfreq.java
@@ -1,5 +1,18 @@
-package DSA_java.Arrays;
+for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
 
-public class longestsubstringwithkleastfreq {
-    
-}
+        // Find characters whose frequency is less than k
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            if (map.get(ch) < k) {
+                int left = longestSubstring(s.substring(0, i), k);
+                int right = longestSubstring(s.substring(i + 1), k);
+
+                maxlen = Math.max(left, right);
+
+                return maxlen;
+            }
+        }
