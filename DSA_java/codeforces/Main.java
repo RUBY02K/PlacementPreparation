30791bde646aabcb38cd645f797ae81fe620c566
Ruby kushwah <rkushwah9826@gmail.com>
2026-08-15 23:35:13 +0530

    codeforces day1

---
+++ DSA_java/codeforces/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+
+            System.out.println(Math.min(a, b));
+        }
+    }
+}
---
+++ DSA_java/codeforces/cf.java
@@ -0,0 +1,5 @@
+package DSA_java.codeforces;
+
+public class cf {
+    
+}
