48850cae55b24559708f0ef2227e04cdc710b77d
Ruby kushwah <rkushwah9826@gmail.com>
2026-08-18 23:49:14 +0530

    misiisng element in the subset maximum

--- DSA_java/Arrays/camdiesw.java
+++ DSA_java/Arrays/camdiesw.java
@@ -1,5 +1,19 @@
-package DSA_java.Arrays;
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> list = new ArrayList<>();
 
-public class camdiesw {
-    
+        int greatest = candies[0];
+        for (int i = 1; i < candies.length; i++) {
+            greatest = Math.max(greatest, candies[i]);
+        }
+
+        for(int  i = 0 ; i < candies.length ; i++){
+         int sum = candies[i]+extraCandies;
+         if(sum >= greatest){
+            list.add(true);
+         }
+         else list.add(false);
+        }
+        return list;
+    }
 }
